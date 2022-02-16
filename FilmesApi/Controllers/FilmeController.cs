72ee93040af1b8e0c7fbb53ac4532015f319a4e7
Rennan Alves <rennan_areas@hotmail.com>
2022-02-16 15:41:12 -0300

    Initial Commit

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public void AdicionarFilmes([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperarFilmes()
+        {
+            return filmes;
+        }
+
+        [HttpGet("{id}")]
+        public Filme RecuperarFilmesPorId(int id)
+        {
+            /*
+            foreach (Filme filme in filmes)
+            {
+                if (filme.Id == id)
+                {
+                    return filme;
+                }
+            }
+
+            return null;
+            */
+
+            return filmes.FirstOrDefault(filme => filme.Id == id);
+        }
+    }
+}
